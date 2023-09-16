0dfde7fa507832fdd9a506b62cd573111dac5456
LiziAC05 <lizbeth.aquisse@pucp.edu.pe>
2023-09-16 08:09:43 -0500

    Enlaces de la vista de delegado de actividad. falta de escoger apoyos

--- app/src/main/java/com/example/weeking/VistaDelegadoActividadActivity.java
+++ app/src/main/java/com/example/weeking/VistaDelegadoActividadActivity.java
@@ -2,7 +2,10 @@ package com.example.weeking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class VistaDelegadoActividadActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class VistaDelegadoActividadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista_delegado_actividad);
+        Button btnActividad =  findViewById(R.id.btnAssignActivity);
+        btnActividad.setOnClickListener(v -> {
+          Intent intent =  new Intent(VistaDelegadoActividadActivity.this, ActividadesActivity.class);
+          startActivity(intent);
+          finish();
+        });
+
+       /* Button btnActividad =  findViewById(R.id.btnAssignActivity);
+        btnActividad.setOnClickListener(v -> {
+            Intent intent =  new Intent(VistaDelegadoActividadActivity.this, ActividadesActivity.class);
+            startActivity(intent);
+            finish();
+        });*/
+
+        Button btnDonacion =  findViewById(R.id.btnDonaDA);
+        btnDonacion.setOnClickListener(v -> {
+            Intent intent =  new Intent(VistaDelegadoActividadActivity.this, Donacion.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
