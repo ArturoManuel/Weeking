97b4eba4c7f851280f5b8de820aecbca96eb6601
ArturoManuel <noriegaarturonoriega@gmail.com>
2023-09-30 08:14:16 -0500

    Mejorando la vista Princiapl

--- app/src/main/java/com/example/weeking/PerfilActivity.java
+++ app/src/main/java/com/example/weeking/PerfilActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
@@ -24,45 +25,44 @@ public class PerfilActivity extends AppCompatActivity {
         TextView volver =findViewById(R.id.volver);
         volver.setOnClickListener(v -> finish());
 
-        meowBottomNavigation = findViewById(R.id.bottomNavigation);
-        meowBottomNavigation.show(1,true);
-        meowBottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.baseline_person_24));
-        meowBottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.baseline_chat_bubble_24));
-        meowBottomNavigation.add(new MeowBottomNavigation.Model(3,R.drawable.baseline_maps_ugc_24));
-        meowBottomNavigation.add(new MeowBottomNavigation.Model(4,R.drawable.baseline_photo_camera_24));
-        meowBottomNavigation.add(new MeowBottomNavigation.Model(5,R.drawable.baseline_house_24));
-        meownavegation();
+
+        navbarnavegation();
     }
 
-    private void meownavegation(){
 
-        meowBottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
-            @Override
-            public Unit invoke(MeowBottomNavigation.Model model) {
-                switch (model.getId()){
-                    case 5:
-                        intent = new Intent(PerfilActivity.this, VistaPrincipal.class);
-                        startActivity(intent);
-                        meowBottomNavigation.show(1,false);
-                        break;
-                    case 2:
-                        intent = new Intent(PerfilActivity.this, Listchats.class);
-                        startActivity(intent);
-                        meowBottomNavigation.show(2,false);
-                        break;
-                    case 3:
-                        intent = new Intent(PerfilActivity.this, MapaActivity.class);
-                        startActivity(intent);
-                        meowBottomNavigation.show(3,false);
-                        break;
-                    case 4:
-                        Toast toast = Toast.makeText(PerfilActivity.this, "Todavía no realizamos  la camara", Toast.LENGTH_LONG);
-                        toast.show();
-                        meowBottomNavigation.show(4,false);
-                        break;
-                }
-                return null;
+    private void navbarnavegation(){
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_profile);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.bottom_profile) {
+                return true;
+            } else if (item.getItemId() == R.id.bottom_camara) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Todavía no realizamos la cámara", Toast.LENGTH_LONG);
+                toast.show();
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.bottom_maps) {
+                startActivity(new Intent(getApplicationContext(), MapaActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.bottom_home) {
+                startActivity(new Intent(getApplicationContext(), VistaPrincipal.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.bottom_chat) {
+                startActivity(new Intent(getApplicationContext(), Listchats.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
             }
+
+
+            return false;
         });
+
     }
 }
