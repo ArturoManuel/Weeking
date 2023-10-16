83d1dd87d0a74a5a58c2e338be72cd125be27374
raykevinconcha <a20186137@pucp.edu.pe>
2023-10-15 19:43:04 -0500

    Guardar Status de Estudiante o Egresado

--- app/src/main/java/com/example/weeking/workers/StatusActivity.java
+++ app/src/main/java/com/example/weeking/workers/StatusActivity.java
@@ -2,19 +2,49 @@ package com.example.weeking.workers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.weeking.R;
 
 public class StatusActivity extends AppCompatActivity {
-
+    private RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+        radioGroup=findViewById(R.id.radioGroup);
+        int savedSelection = getSavedSelection();
 
+        if (savedSelection != -1) {
+            radioGroup.check(savedSelection);
+        }
+        findViewById(R.id.btnSaveStatus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedRadioButtonId = radioGroup.getCheckedRadioButtonId();
+                if(selectedRadioButtonId != -1){
+                    savedSelection(selectedRadioButtonId);
+                    Toast.makeText(StatusActivity.this, "Estado guardado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         TextView volver =findViewById(R.id.volver);
         volver.setOnClickListener(v -> finish());
+    }
+
+    private int getSavedSelection() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPreferences", MODE_PRIVATE);
+        return sharedPreferences.getInt("SelectedRadioButtonId", -1);
+    }
+    private void savedSelection(int selectedRadioButtonId){
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPreferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("SelectedRadioButtonId", selectedRadioButtonId);
+        editor.apply();
     }
 }
