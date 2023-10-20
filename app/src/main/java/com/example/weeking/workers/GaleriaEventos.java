e16ec76f6564c7b1e80f9ebfb7c05d8a4bfbe6a9
raykevinconcha <a20186137@pucp.edu.pe>
2023-10-19 23:02:54 -0500

    Galeria de Eventos

--- app/src/main/java/com/example/weeking/workers/GaleriaEventos.java
+++ app/src/main/java/com/example/weeking/workers/GaleriaEventos.java
@@ -2,15 +2,31 @@ package com.example.weeking.workers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 import com.example.weeking.R;
+import com.example.weeking.workers.adaptador.GaleriaFotosAdapter;
 
 public class GaleriaEventos extends AppCompatActivity {
-
+    GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galeria_eventos);
+        gridView=(GridView) findViewById(R.id.gv_imagenes);
+        gridView.setAdapter(new GaleriaFotosAdapter(this));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent=new Intent(getApplicationContext(), imagen_completa.class);
+                intent.putExtra("misImagenes",position);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/weeking/workers/VistaEventoActivity.java
+++ app/src/main/java/com/example/weeking/workers/VistaEventoActivity.java
@@ -2,7 +2,10 @@ package com.example.weeking.workers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.weeking.R;
@@ -13,6 +16,14 @@ public class VistaEventoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista_evento);
+        Button btnVerFotos = findViewById(R.id.btnDonaciones);
+        btnVerFotos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VistaEventoActivity.this, GaleriaEventos.class);
+                startActivity(intent);
+            }
+        });
 
         TextView volver =findViewById(R.id.volver);
         volver.setOnClickListener(v -> finish());
