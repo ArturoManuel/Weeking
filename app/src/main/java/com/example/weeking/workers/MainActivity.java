65f0538f2d2a5ea7b6c1c5e830391cff573ce20d
ArturoManuel <noriegaarturonoriega@gmail.com>
2023-10-21 09:04:10 -0500

    Notificación de bienvenida

--- app/src/main/java/com/example/weeking/workers/MainActivity.java
+++ app/src/main/java/com/example/weeking/workers/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.weeking.workers;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.POST_NOTIFICATIONS;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.weeking.R;
 import com.example.weeking.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -18,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
 
+    String canal1 = "importanteDefault";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         auth = FirebaseAuth.getInstance();
+
+        crearCanalesNotificacion();
 
 
         binding.iniciarSesion.setOnClickListener(v -> {
@@ -35,7 +52,10 @@ public class MainActivity extends AppCompatActivity {
                 if (!contrasena.isEmpty()) {
                     auth.signInWithEmailAndPassword(correo, contrasena)
                             .addOnSuccessListener(authResult -> {
+
                                 Toast.makeText(MainActivity.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+                                notificarImportanceDefault();
+
                                 startActivity(new Intent(MainActivity.this, VistaPrincipal.class));
                                 finish();
                             })
@@ -53,8 +73,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        binding.recuperarContrasena.setOnClickListener(v-> navigateToActivity(ContrasenaRecuperacion_Activity.class));
-        binding.registrate.setOnClickListener(v->navigateToActivity(RegistroActivity.class));
+        binding.recuperarContrasena.setOnClickListener(v -> navigateToActivity(ContrasenaRecuperacion_Activity.class));
+        binding.registrate.setOnClickListener(v -> navigateToActivity(RegistroActivity.class));
         binding.imageView.setOnClickListener(v -> navigateToActivity(ActividadesActivity.class));
         binding.bienvenidos.setOnClickListener(v -> navigateToActivity(Lista_don.class));
     }
@@ -63,5 +83,54 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, destinationClass);
         startActivity(intent);
     }
+
+    public void crearCanalesNotificacion() {
+
+        NotificationChannel channel = new NotificationChannel(canal1,
+                "Canal notificaciones default",
+                NotificationManager.IMPORTANCE_DEFAULT);
+        channel.setDescription("Canal para notificaciones con prioridad default");
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+
+        pedirPermisos();
+    }
+    public void pedirPermisos() {
+        // TIRAMISU = 33
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+                ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{POST_NOTIFICATIONS}, 101);
+        }
+    }
+
+    private void notificarImportanceDefault() {
+        // Crear notificación
+        Intent intent = new Intent(this, VistaPrincipal.class); // Cambia esto según donde quieras que vaya el usuario al hacer clic en la notificación
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, canal1)
+                .setSmallIcon(R.drawable.logo_48) // Cambia esto por tu propio icono
+                .setContentTitle("Bienvenidos a la weeking")  // Título modificado
+                .setContentText("Gracias por unirte a nosotros") // Texto modificado
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+
+        Notification notification = builder.build();
+
+        // Lanzar notificación
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        if (ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(1, notification);
+        }
+
+    }
+
+
+
 
 }
