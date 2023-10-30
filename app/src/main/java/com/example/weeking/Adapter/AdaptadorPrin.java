8cb713937c9fb31eb54ca81847586868da5b65f8
ArturoManuel <noriegaarturonoriega@gmail.com>
2023-10-30 16:59:50 -0500

    cambios

--- app/src/main/java/com/example/weeking/Adapter/AdaptadorPrin.java
+++ app/src/main/java/com/example/weeking/Adapter/AdaptadorPrin.java
@@ -107,21 +107,21 @@ public class AdaptadorPrin extends RecyclerView.Adapter<AdaptadorPrin.ViewHolder
                     .load(imageUrl)
                     .into(foto);  // `foto` es tu ImageView
 
-            if (!item.getLike()) {
-                likeButton.setImageResource(R.drawable.ic_outline_heart);  // Cambia ic_not_liked por tu ícono de like no activo
-            } else {  // Cambia isEstado() por el método que verifica si tiene like o no
-                likeButton.setImageResource(R.drawable.ic_filled_heart);  // Cambia ic_liked por tu ícono de like activo
-            }
-
-            likeButton.setOnClickListener(v -> {
-                item.toggleLike(); // Este método cambiará el estado de "like" del evento
-                updateLikeInFirebase(item.getEventId(), item.isEstado()); // Asumiendo que tienes un método getId() en tu clase EventoClass para obtener el ID único del evento.
-                if(item.getLike()) {
-                    likeButton.setImageResource(R.drawable.ic_filled_heart);
-                } else {
-                    likeButton.setImageResource(R.drawable.ic_outline_heart);
-                }
-            });
+//            if (!item.getLike()) {
+//                likeButton.setImageResource(R.drawable.ic_outline_heart);  // Cambia ic_not_liked por tu ícono de like no activo
+//            } else {  // Cambia isEstado() por el método que verifica si tiene like o no
+//                likeButton.setImageResource(R.drawable.ic_filled_heart);  // Cambia ic_liked por tu ícono de like activo
+//            }
+//
+//            likeButton.setOnClickListener(v -> {
+//                item.toggleLike(); // Este método cambiará el estado de "like" del evento
+//                updateLikeInFirebase(item.getEventId(), item.isEstado()); // Asumiendo que tienes un método getId() en tu clase EventoClass para obtener el ID único del evento.
+//                if(item.getLike()) {
+//                    likeButton.setImageResource(R.drawable.ic_filled_heart);
+//                } else {
+//                    likeButton.setImageResource(R.drawable.ic_outline_heart);
+//                }
+//            });
 
         }
 
