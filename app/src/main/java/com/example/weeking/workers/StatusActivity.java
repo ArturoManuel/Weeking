77171ca94c9fe9b75423c48e3828c4be8c18b8fb
weekings <a20190212@pucp.edu.pe>
2023-12-01 23:52:05 -0500

    kit de donacion

--- app/src/main/java/com/example/weeking/workers/StatusActivity.java
+++ app/src/main/java/com/example/weeking/workers/StatusActivity.java
@@ -25,6 +25,9 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.squareup.picasso.Picasso;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StatusActivity extends AppCompatActivity {
     FirebaseFirestore db;
     private Button btnAdquiereKit;
@@ -37,7 +40,7 @@ public class StatusActivity extends AppCompatActivity {
         ImageView imagen = findViewById(R.id.imageView4);
         TextView monto = findViewById(R.id.Donacion);
         TextView motivo = findViewById(R.id.Donacion1);
-        btnListaDonaciones = findViewById(R.id.btnListaDonar);
+        final String[] codigo = {null};
         btnAdquiereKit = findViewById(R.id.btnKitAccess);
         Query query = db.collection("usuarios").whereEqualTo("authUID", FirebaseAuth.getInstance().getCurrentUser().getUid());
         query.get().addOnCompleteListener(task -> {
@@ -45,41 +48,19 @@ public class StatusActivity extends AppCompatActivity {
                 QuerySnapshot queryDocumentSnapshot = task.getResult();
                 if (!queryDocumentSnapshot.isEmpty()) {
                     DocumentSnapshot document = queryDocumentSnapshot.getDocuments().get(0);
-                    Log.d("aaaaa", "adas");
                     Query query1 = db.collection("donaciones").whereEqualTo("codigo", document.getString("codigo"));
                     query1.get().addOnCompleteListener(task1 -> {
                         if (task1.isSuccessful()) {
                             QuerySnapshot queryDocumentSnapshot1 = task1.getResult();
-                            Log.d("aaaaa", "adas");
                             if (!queryDocumentSnapshot1.isEmpty()) {
-                                Log.d("aaaaa", "adas1");
                                 DocumentSnapshot document1 = queryDocumentSnapshot1.getDocuments().get(0);
                                 if (document1.getData() != null) {
-                                    Log.d("aaaaa", "adas2");
                                     ListaDon dona = document1.toObject(ListaDon.class);
-                                    Log.d("aaaaa", "adas3");
                                     monto.setText(dona.getMonto() + " soles");
-                                    Query query2 = db.collection("usuarios").whereEqualTo("authUID",FirebaseAuth.getInstance().getCurrentUser().getUid());
-                                    query2.get().addOnCompleteListener(task2 ->{
-                                        if(task2.isSuccessful()) {
-                                            QuerySnapshot queryDocumentSnapshot2 = task.getResult();
-                                            if (!queryDocumentSnapshot2.isEmpty()) {
-                                                DocumentSnapshot document2 = queryDocumentSnapshot.getDocuments().get(0);
-                                                String estado = document2.getString("estado");
-                                                Log.d("dic", estado);
-                                                if (estado.equals("Egresado")){
-                                                    Log.d("msg", "es egresado");
-                                                }
-                                            }
-                                        }
-                                        });
-                                    if (dona.getMonto() >= 200) {
+                                    codigo[0] = dona.getCodigo();
+                                    if (dona.getMonto() >= 200 && dona.isEgresado()) {
                                         btnAdquiereKit.setVisibility(View.VISIBLE);
-                                    } else {
-                                        btnAdquiereKit.setVisibility(View.INVISIBLE);
                                     }
-
-                                    //Verifica para que se active el boton de adquirir kit (falta relacionarlo con los egresados)
                                     String rechazo = document1.getString("rechazo");
                                     if (rechazo.equals("1")) {
                                         Picasso.get().load(dona.getFoto()).into(imagen);
@@ -91,7 +72,6 @@ public class StatusActivity extends AppCompatActivity {
                                         motivo.setText("Ultima Donacion ha sido rechazado por esto:\n" + rechazo);
                                     }
                                 } else {
-                                    Log.d("aaaaa", "adas5");
                                     motivo.setText("No has realizado ninguna donacion");
                                     imagen.setVisibility(View.GONE);
                                 }
@@ -103,12 +83,15 @@ public class StatusActivity extends AppCompatActivity {
             }
         });
 
-
-
-        ///para acceder a la lista de donaciones
-        btnListaDonaciones.setOnClickListener(view -> {
-            Intent intent = new Intent(StatusActivity.this, ListaDonacionesActivity.class);
-            startActivity(intent);
+        btnAdquiereKit.setOnClickListener(view -> {
+            Map<String, Object> map1 = new HashMap<>();
+            map1.put("notifi","El alumno con codigo "+ codigo[0] +" ha solicitado un kit");
+            map1.put("codigo","20190411");
+            db.collection("noti").add(map1);
+            Map<String, Object> map = new HashMap<>();
+            map.put("egresado",false);
+            db.collection("donaciones").document(codigo[0]).update(map);
+            finish();
         });
 
 
