07f9bd95f6ae34db383d25d05929bfa0fa80156f
weekings <a20190212@pucp.edu.pe>
2023-12-10 23:56:55 -0500

    mapa

--- app/src/main/java/com/example/weeking/workers/fragmentos/mapafragmento.java
+++ app/src/main/java/com/example/weeking/workers/fragmentos/mapafragmento.java
@@ -9,10 +9,15 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.example.weeking.R;
 import com.example.weeking.entity.EventoClass;
 import com.example.weeking.entity.ListaDon;
 import com.example.weeking.entity.ListaEven;
+import com.example.weeking.workers.viewModels.AppViewModel;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -33,6 +38,14 @@ public class mapafragmento extends Fragment implements OnMapReadyCallback {
 
     private OnMapReadyListener callback;
     private GoogleMap mMap;
+
+    private AppViewModel appViewModel;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        appViewModel = new ViewModelProvider(requireActivity()).get(AppViewModel.class);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_mapafragmento, container, false);
@@ -64,24 +77,15 @@ public class mapafragmento extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        FirebaseFirestore db;
-        db = FirebaseFirestore.getInstance();
-        db.collection("Eventos").addSnapshotListener((collection, error) -> {
-            if (error != null) {
-                Log.d("lectura", "Error listening for document changes.");
-                return;
-            }
-            if (collection != null && !collection.isEmpty()) {
-                Log.d("asdf","a");
-                for (QueryDocumentSnapshot document : collection) {
-                    EventoClass dona = document.toObject(EventoClass.class);
-                    LatLng a = new LatLng(dona.getLatitud(), dona.getLongitud());
-                    Marker marker = mMap.addMarker(new MarkerOptions().position(a).title(dona.getDescripcion()));
+        appViewModel.getListaEventos().observe(getViewLifecycleOwner(), eventoClasses -> {
+            if (eventoClasses != null && !eventoClasses.isEmpty()) {
+                for (EventoClass evento : eventoClasses) {
+                    LatLng a = new LatLng(evento.getLatitud(), evento.getLongitud());
+                    Marker marker = mMap.addMarker(new MarkerOptions().position(a).title(evento.getDescripcion()));
                     if (marker != null) {
                         marker.showInfoWindow();
                     }
                 }
-
             }
         });
         LatLng defaultLocation = new LatLng(-12.072257, -77.079859);
