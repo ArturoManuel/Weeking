5644f130f8f0b2518bc24dee27859129739f316a
ArturoManuel <noriegaarturonoriega@gmail.com>
2023-12-10 23:54:48 -0500

    prueba

--- app/src/main/java/com/example/weeking/workers/fragmentos/mapafragmento.java
+++ app/src/main/java/com/example/weeking/workers/fragmentos/mapafragmento.java
@@ -9,10 +9,14 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.weeking.Adapter.AdaptadorPrin;
 import com.example.weeking.R;
 import com.example.weeking.entity.EventoClass;
 import com.example.weeking.entity.ListaDon;
 import com.example.weeking.entity.ListaEven;
+import com.example.weeking.workers.viewModels.AppViewModel;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -33,17 +37,38 @@ public class mapafragmento extends Fragment implements OnMapReadyCallback {
 
     private OnMapReadyListener callback;
     private GoogleMap mMap;
+
+    private AppViewModel appViewModel;
+
+    private  List<EventoClass> listaeventos;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        appViewModel = new ViewModelProvider(requireActivity()).get(AppViewModel.class);
+
+    }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mapafragmento, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_mapafragmento, container, false);
+
+        return view;
+
+
+
     }
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // Initialize map fragment
-
-
         view.post(() -> {
+
+            appViewModel.getListaEventos().observe(getViewLifecycleOwner(), eventoClasses -> {
+               Log.d("eventos_mapa",eventoClasses.get(0).getEventId()+eventoClasses.get(0).getLongitud());
+            });
             SupportMapFragment mapFragment = SupportMapFragment.newInstance();
             getChildFragmentManager().beginTransaction().replace(R.id.mapFragment, mapFragment).commit();
             mapFragment.getMapAsync(this);
@@ -64,26 +89,6 @@ public class mapafragmento extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        FirebaseFirestore db;
-        db = FirebaseFirestore.getInstance();
-        db.collection("Eventos").addSnapshotListener((collection, error) -> {
-            if (error != null) {
-                Log.d("lectura", "Error listening for document changes.");
-                return;
-            }
-            if (collection != null && !collection.isEmpty()) {
-                Log.d("asdf","a");
-                for (QueryDocumentSnapshot document : collection) {
-                    EventoClass dona = document.toObject(EventoClass.class);
-                    LatLng a = new LatLng(dona.getLatitud(), dona.getLongitud());
-                    Marker marker = mMap.addMarker(new MarkerOptions().position(a).title(dona.getDescripcion()));
-                    if (marker != null) {
-                        marker.showInfoWindow();
-                    }
-                }
-
-            }
-        });
         LatLng defaultLocation = new LatLng(-12.072257, -77.079859);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, 15));
 
